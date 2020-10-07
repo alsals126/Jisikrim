4f3087bc128cd730ea1576a8d67557360aaedee5
alsals126 <9970mia@naver.com>
2020-10-07 23:41:40 +0900

    201007

--- app/src/main/java/kr/hs/emirim/s2019s33/mirimjisik/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/s2019s33/mirimjisik/MainActivity.java
@@ -4,11 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
-import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,30 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//
-//        RadioGroup rg = (RadioGroup) findViewById(R.id.radioGroup);
-//
-//        rg.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-//            @Override
-//            public void onCheckedChanged(RadioGroup group, int checkedId) {
-//                int result;
-//                Spinner spinner = (Spinner) findViewById(R.id.subjects);
-//                ArrayAdapter<CharSequence> adapter;
-//
-//                if(checkedId == R.id.Sophomore)
-//                    result = R.array.SecondSubjects;
-//                else if(checkedId == R.id.Junior)
-//                    result = R.array.ThirdnSubjects;
-//                else
-//                    result = R.array.FreshmanSubjects;
-//
-//                adapter= ArrayAdapter.createFromResource(
-//                        getApplicationContext(), result, android.R.layout.simple_spinner_item
-//                );
-//                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-//                spinner.setAdapter(adapter);
-//            }
-//        });
 
         Button btnNew=(Button)findViewById(R.id.btn_new);
         Button btnLogin=(Button)findViewById(R.id.btn_login);
@@ -60,6 +32,5 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
     }
 }
--- app/src/main/java/kr/hs/emirim/s2019s33/mirimjisik/inputquestion_Activity.java
+++ app/src/main/java/kr/hs/emirim/s2019s33/mirimjisik/inputquestion_Activity.java
@@ -1,22 +1,38 @@
 package kr.hs.emirim.s2019s33.mirimjisik;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.RadioButton;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
+import java.io.InputStream;
 
 public class inputquestion_Activity extends AppCompatActivity {
+
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.inputquestion_layout);
 
-        RadioGroup rg = (RadioGroup) findViewById(R.id.radioGroup);
+        Button inputB = (Button) findViewById(R.id.inputButton);
+        inputB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(inputquestion_Activity.this, questionActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //라디오버튼튼
+       RadioGroup rg = (RadioGroup) findViewById(R.id.radioGroup);
 
         rg.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
@@ -40,5 +56,35 @@ public class inputquestion_Activity extends AppCompatActivity {
             }
         });
 
+        //이미지 불러오기
+        imageView = (ImageView) findViewById(R.id.imageView);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(intent, 1);
+            }
+        });
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == 1){
+            if(resultCode == RESULT_OK){
+                try{
+                    InputStream in = getContentResolver().openInputStream(data.getData());
+                    Bitmap img = BitmapFactory.decodeStream(in);
+                    in.close();
+                    imageView.setImageBitmap(img);
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
 }

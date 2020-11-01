eb9c8d4389c9db20da3775d1ee4f547a1442ef48
alsals126 <9970mia@naver.com>
2020-11-01 17:34:35 +0900

    Checked DATABASE Connection

--- app/src/main/java/kr/hs/emirim/s2019s33/mirimjisik/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/s2019s33/mirimjisik/MainActivity.java
@@ -6,7 +6,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+/*public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +33,31 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, mainviewActivity.class);
                 startActivity(intent);
+            }
+        });
+    }
+
+}*/
+
+// 데이터베이스 연결 확인
+public class MainActivity extends AppCompatActivity {
+
+    private Button sendbt;
+
+    private FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+    private DatabaseReference databaseReference = firebaseDatabase.getReference();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        sendbt = (Button) findViewById(R.id.button);
+
+        sendbt.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                // 버튼 누르면 수행 할 명령
+                databaseReference.child("message").push().setValue("2");
             }
         });
     }

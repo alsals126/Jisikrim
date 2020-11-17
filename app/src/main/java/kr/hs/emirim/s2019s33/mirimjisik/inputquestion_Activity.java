84b138d6f1030d2f99726add48fc769de05ef2b6
alsals126 <9970mia@naver.com>
2020-11-17 23:58:32 +0900

    혹시 몰라서 백업(20.11.17 23:58)

--- app/src/main/java/kr/hs/emirim/s2019s33/mirimjisik/inputquestion_Activity.java
+++ app/src/main/java/kr/hs/emirim/s2019s33/mirimjisik/inputquestion_Activity.java
@@ -1,53 +1,86 @@
 package kr.hs.emirim.s2019s33.mirimjisik;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.text.Editable;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.Exclude;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import org.w3c.dom.Text;
+
 import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
 
 public class inputquestion_Activity extends AppCompatActivity {
 
+    private static final String TAG = "inputquestion_Activity";
+    private FirebaseUser user;
     private ImageView imageView;
+//    private EditText textTitle, textContent;
+    private RadioGroup rg;
+    Button inputB;
+//    int grade;
+//    String subject;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.inputquestion_layout);
 
-        Button inputB = (Button) findViewById(R.id.inputButton);
-        inputB.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent=new Intent(inputquestion_Activity.this, my.class);
-                startActivity(intent);
-            }
-        });
+//        // 초기화
+//        textTitle = (EditText)findViewById(R.id.editText);
+        rg = findViewById(R.id.radioGroup);
 
-        //라디오버튼튼
-       RadioGroup rg = (RadioGroup) findViewById(R.id.radioGroup);
+        imageView = (ImageView) findViewById(R.id.imageView);
+//        textContent = (EditText) findViewById(R.id.edit_content);
+        inputB = (Button) findViewById(R.id.inputButton);
 
+        //라디오버튼
         rg.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
                 int result;
-                Spinner spinner = (Spinner) findViewById(R.id.subjects);
+                Spinner spinner = findViewById(R.id.subjects);
+
                 ArrayAdapter<CharSequence> adapter;
 
-                if(checkedId == R.id.Sophomore)
+                if(checkedId == R.id.Sophomore){
                     result = R.array.SecondSubjects;
-                else if(checkedId == R.id.Junior)
+                }
+                else if(checkedId == R.id.Junior){
                     result = R.array.ThirdnSubjects;
-                else
+                }
+                else{
                     result = R.array.FreshmanSubjects;
-
+                }
                 adapter= ArrayAdapter.createFromResource(
                         getApplicationContext(), result, android.R.layout.simple_spinner_item
                 );
@@ -57,7 +90,6 @@ public class inputquestion_Activity extends AppCompatActivity {
         });
 
         //이미지 불러오기
-        imageView = (ImageView) findViewById(R.id.imageView);
         imageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -67,6 +99,75 @@ public class inputquestion_Activity extends AppCompatActivity {
                 startActivityForResult(intent, 1);
             }
         });
+
+        inputB.setOnClickListener(onClickListener);
+
+        /*Board b = new Board(textTitle.getText().toString(), grade, subject, textContent.getText().toString());*/
+        /*b.toMap();*/
+        // db에 넣기
+    /*    final FirebaseFirestore db = FirebaseFirestore.getInstance();
+        Map<String, Object> user = new HashMap<>();
+        user.put("title", textTitle.getText().toString());
+        user.put("last","sibal");
+        user.put("born", 1815);*/
+
+    /*    db.collection("users").add(user)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d(TAG, "add" + documentReference.get());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "eROOD", e);
+                    }
+                });*/
+
+        /*textTitle = (EditText) findViewById(R.id.textView4);*/
+        /*textContent = (TextView) findViewById(R.id.textView6);*/
+
+       /* inputB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final FirebaseFirestore db = FirebaseFirestore.getInstance();
+                Map<String, Object> user = new HashMap<>();
+                user.put("first", ((EditText) findViewById(R.id.editText)).getText().toString());
+                user.put("last","sibal");
+                user.put("born", 1815);
+                db.collection("users").add(user)
+                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                            @Override
+                            public void onSuccess(DocumentReference documentReference) {
+                                Log.d(TAG, "DocumentSnapshot written with ID: " + documentReference.getId());
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.w(TAG, "Error adding document", e);
+                            }
+                        });
+                *//*db.collection("users").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if(task.isSuccessful()){
+                            for(QueryDocumentSnapshot document : task.getResult()){
+                                Log.d(TAG, document.getId() + "=>" + document.getData());
+                            }
+                        }else{
+                            Log.w(TAG, "ERRO", task.getException());
+                        }
+                    }
+                });*//*
+               *//* Intent intent = new Intent(getApplicationContext(), basic.class);
+                startActivity(intent);*//*
+                startActivity(new Intent(getApplicationContext(), basic.class));
+                *//*Intent intent=new Intent(inputquestion_Activity.this, my.class);
+                startActivity(intent);*//*
+            }
+        });*/
     }
 
     @Override
@@ -87,4 +188,82 @@ public class inputquestion_Activity extends AppCompatActivity {
         }
     }
 
+    View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()){
+                case R.id.inputButton:
+                    profileUpdate();
+                    break;
+            }
+        }
+    };
+    private void profileUpdate() {
+        String textTitle = ((EditText) findViewById(R.id.editText)).getText().toString();
+        String textContent = ((EditText) findViewById(R.id.edit_content)).getText().toString();
+        RadioButton rd = (RadioButton)findViewById(rg.getCheckedRadioButtonId());
+        String grade = rd.getText().toString();
+
+        if (textTitle.length() > 0 && textContent.length() > 0) {
+            user = FirebaseAuth.getInstance().getCurrentUser();
+            WriteInfo writeInfo = new WriteInfo(textTitle, grade, textContent, user.getUid());
+            uploader(writeInfo);
+        } else {
+            Toast.makeText(this, "회원정보를 입력해주세요", Toast.LENGTH_SHORT).show();
+        }
+    }
+    private void uploader(WriteInfo writeInfo){
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.collection("posts").add(writeInfo)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d(TAG, "DocumentSnapshot written with ID: " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error adding document", e);
+                    }
+                });
+    }
+}
+class WriteInfo {
+    private String title;
+    private String grade;
+    private String contents;
+    private String publisher;
+
+    public WriteInfo(String title, String grade, String contents, String publisher){
+        this.title = title;
+        this.grade = grade;
+        this.contents = contents;
+        this.publisher = publisher;
+    }
+
+    public String getTitle(){
+        return this.title;
+    }
+    public void setTitle(String title){
+        this.title = title;
+    }
+    public String getGrade(){
+        return this.grade;
+    }
+    public void setGrade(String grade){
+        this.grade = grade;
+    }
+    public String getContents(){
+        return this.contents;
+    }
+    public void setContents(String contents){
+        this.contents = contents;
+    }
+    public String getPublisher(){
+        return this.publisher;
+    }
+    public void setPublisher(String publisher){
+        this.publisher = publisher;
+    }
 }
